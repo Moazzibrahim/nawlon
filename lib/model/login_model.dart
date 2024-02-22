6134216bff74e04e1bce7c32d83f393d473d6e28
Moaz Ibrahim <98399366+wezza99@users.noreply.github.com>
2024-02-22 12:20:18 +0200

    elkhames3

--- lib/model/login_model.dart
+++ lib/model/login_model.dart
@@ -1,24 +1,28 @@
 // ignore_for_file: use_build_context_synchronously
-
 import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_dashboard/dashboard.dart';
 import 'package:http/http.dart' as http;
 
 class LoginModel with ChangeNotifier {
-  Future<void> loginUser(
-      String email, String password, BuildContext context) async {
+  TextEditingController emailcontroller = TextEditingController();
+  TextEditingController passController = TextEditingController();
+  Future<void> loginUser(BuildContext context) async {
     // API endpoint to authenticate user
     String apiUrl = 'https://yourapi.com/login';
 
     try {
       final response = await http.post(
         Uri.parse(apiUrl),
-        body: {'email': email, 'password': password},
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        },
+        body: {'email': emailcontroller.text, 'password': passController.text},
       );
 
       if (response.statusCode == 200) {
+        //final Map<String, dynamic> responseData = jsonDecode(response.body);
         // Authentication successful
         Navigator.pushReplacement(
           context,
